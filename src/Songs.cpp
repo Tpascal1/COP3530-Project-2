22c71538d07fe0e0c7020cb6279aa7f6fc9d08de
Tatiana Pascal <Tpascal1@ufl.edu>
2025-10-27 21:48:21 -0400

    Initial project setup

---
+++ src/Songs.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tatia on 10/23/2025.
+//
+
+#include "Songs.h"
---
+++ src/Songs.h
@@ -0,0 +1,16 @@
+//
+// Created by Tatia on 10/23/2025.
+//
+
+#ifndef SONGS_H
+#define SONGS_H
+
+
+
+class Songs {
+
+};
+
+
+
+#endif //SONGS_H
