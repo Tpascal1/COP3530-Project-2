368dd70ead10b4c57ab285bc65c2e8adad577a11
deshnanaruka <d35hnanaruka@gmail.com>
2025-11-02 21:56:29 -0500

    Implemented Merge Sort for songs (descending hotness) and expanded dataset to 100k for runtime test

--- include/SortMerge.h
+++ include/SortMerge.h
@@ -1,3 +1,19 @@
 //
 // Created by Deshna on 10/29/2025.
 //
+
+#ifndef SORTMERGE_H
+#define SORTMERGE_H
+
+#include <vector>
+#include "Songs.h"
+using namespace std ;
+
+void mergeSort(vector<Songs>& songs);
+void merge(vector<Songs>& songs, int left, int mid, int right);
+void mergeSortHelper(vector<Songs>& songs, int left, int right);
+
+
+
+
+#endif //SORTMERGE_H
--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,54 @@
 
 #include <iostream>
 #include <vector>
+#include <chrono>
 #include "../include//Songs.h"
 #include "../include//io.h"
-#include "../include//SortHeap.h"
+#include "../include//SortMerge.h"
 using namespace std;
+using namespace std::chrono; 
 
 int main() {
+
     cout << "Loading songs is started" << endl;
-
     vector<Songs> songs = loadSongs("../../data/music.json");
-
     cout << "Total songs loaded is: " << songs.size() << endl;
 
-    for (int i = 0; i < 5 && i < songs.size(); i++) {
-        cout << i + 1 << ". "
-            << songs[i].artist << " - "
-            << songs[i].title
-            << " Hotness: " << songs[i].hotness << endl;
+    ////duplicating until we get to 100k songs
+    int target = 100000;
+    int ogsize = songs.size();
+    while (songs.size() < target) {
+        songs.push_back(songs[songs.size() % ogsize]);
     }
+    cout << "The dataset is being expanded to " << songs.size();
+
+
+
+    cout << "\nSorting songs by hotness for Merge Sort:" << endl;
+    auto start = high_resolution_clock::now();
+    mergeSort(songs);
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    cout << "Merge Sorge completed in" << duration.count() << "ms" << endl;
+    
+    cout << "\nTop 5 songs after sorting by descending order:\n";
+    for (int i = 0; i < songs.size(); i++) {
+        cout << i + 1 << ". " << songs[i].artist
+            << " - " << songs[i].title
+            << " | Hotness: " << songs[i].hotness << endl;
+    }
+    // cout << "Loading songs is started" << endl;
+
+    // vector<Songs> songs = loadSongs("../../data/music.json");
+
+    // cout << "Total songs loaded is: " << songs.size() << endl;
+
+    // for (int i = 0; i < 5 && i < songs.size(); i++) {
+    //     cout << i + 1 << ". "
+    //         << songs[i].artist << " - "
+    //         << songs[i].title
+    //         << " Hotness: " << songs[i].hotness << endl;
+    // }
 
     return 0;
 }
