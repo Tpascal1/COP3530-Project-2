1d2132174532d620846be551c5ab06a26c973708
redtinypoop <khuemura@ufl.edu>
2025-11-03 22:09:26 -0500

    Buttons Implemented
    - button images made originally in FIGMA and exported to project
    - incorporated hover and click functionality
    - code can be exited by either closing the window or pressing the ESCAPE key

--- src/UI.cpp
+++ src/UI.cpp
@@ -33,17 +33,127 @@ void launchUI() {
     titleText.setCharacterSize(110);
     titleText.setPosition(75,35);
 
+    //make textures for buttons
+    sf::Texture normalButton;
+    if (!normalButton.loadFromFile("resources/UI_Assets/Button.png")) {
+        std::cout << "Error loading image." << std::endl;
+    }
+    sf::Texture hoverButton;
+    if (!hoverButton.loadFromFile("resources/UI_Assets/Button_Hover.png")) {
+        std::cout << "Error loading image." << std::endl;
+    }
+
+    //Heap Sort Button
+    sf::RectangleShape heapRect(sf::Vector2f(400, 200));
+    heapRect.setTexture(&normalButton);
+    heapRect.setPosition(120, 800);
+    sf::FloatRect heapBounds = heapRect.getGlobalBounds();
+    //make/style text
+    sf::Text heapText;
+    heapText.setFont(font);
+    heapText.setString("Heap\nSort");
+    heapText.setCharacterSize(70);
+    heapText.setPosition(250, 810);
+
+    //Merge-Sort Button
+    sf::RectangleShape mergeRect(sf::Vector2f(400, 200));
+    mergeRect.setTexture(&normalButton);
+    mergeRect.setPosition(760, 800);
+    sf::FloatRect mergeBounds = mergeRect.getGlobalBounds();
+    //make/style text
+    sf::Text mergeText;
+    mergeText.setFont(font);
+    mergeText.setString("Merge\n Sort");
+    mergeText.setCharacterSize(70);
+    mergeText.setPosition(880,810);
+
+    //Both Button
+    sf::RectangleShape bothRect(sf::Vector2f(400, 200));
+    bothRect.setTexture(&normalButton);
+    bothRect.setPosition(1400, 800);
+    sf::FloatRect bothBounds = bothRect.getGlobalBounds();
+    //make/style text
+    sf::Text bothText;
+    bothText.setFont(font);
+    bothText.setString("      Run\nComparison");
+    bothText.setCharacterSize(70);
+    bothText.setPosition(1430,810);
+
+
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Group 42 - Project 2");
     
     while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
- 
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+                window.close();
+            }
+
+            //get mouse position
+            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+            //check mouse click event
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    if (heapBounds.contains(static_cast<sf::Vector2f>(mousePos))) {
+                        //call heap sort function
+                        //this is where we can probably call your guys' function with a certain number, N, of songs given by the user
+                        std::cout << "Heap clicked\n";
+                    }
+                    if (mergeBounds.contains(static_cast<sf::Vector2f>(mousePos))) {
+                        //call merge sort function
+                        //this is where we can probably call your guys' function with a certain number, N, of songs given by the user
+                        std::cout << "Merge clicked\n";
+
+                    }
+                    if (bothBounds.contains(static_cast<sf::Vector2f>(mousePos))) {
+                        //call both functions 
+                        //this is where we can probably call your guys' function with a certain number, N, of songs given by the user
+                        std::cout << "Both clicked\n";
+
+                    }
+                }
+             }
+        }
+
+        //get mouse position
+            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+            //check if mouse is hovering over a button
+            if (heapBounds.contains(static_cast<sf::Vector2f>(mousePos))) {
+                heapRect.setTexture(&hoverButton); 
+            } else {
+                heapRect.setTexture(&normalButton);
+            }
+
+            if (mergeBounds.contains(static_cast<sf::Vector2f>(mousePos))) {
+                mergeRect.setTexture(&hoverButton); 
+            } else {
+                mergeRect.setTexture(&normalButton);
+            }
+
+            if (bothBounds.contains(static_cast<sf::Vector2f>(mousePos))) {
+                bothRect.setTexture(&hoverButton);
+            } else {
+                bothRect.setTexture(&normalButton);
+            }
+        
         window.clear();
         window.draw(title);
         window.draw(titleText);
+
+        window.draw(heapRect);
+        window.draw(heapText);
+
+        window.draw(mergeRect);
+        window.draw(mergeText);
+
+        window.draw(bothRect);
+        window.draw(bothText);
         window.display();
     }
 }
